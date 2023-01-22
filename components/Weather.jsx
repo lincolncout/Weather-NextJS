ba447b97407474f6e62a00b80bf5c979a905cc42
lincolncout <lincolnanc@gmail.com>
2023-01-22 20:46:51 +0100

    Initial commit

---
+++ components/Weather.jsx
@@ -0,0 +1,17 @@
+// rafce atalho
+import React from "react";
+import Image from "next/image";
+
+const Weather = ({ data }) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <Image alt="/"></Image>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
